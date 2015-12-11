70620f24cec148f006a15c62997ae3580f6d9c36
tyson444 <tyson444@yahoo.com>
2015-12-11 13:45:20 +0900

    added print line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Worlld");
+		System.out.println("World's End Dancehall");
 	}
 
 }
